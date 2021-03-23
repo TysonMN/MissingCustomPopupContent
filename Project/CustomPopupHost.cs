5aad1bc4bac11c768502d370ba8fa6dc89f55906
Tyson Williams <34664007+TysonMN@users.noreply.github.com>
2021-03-23 12:29:04 -0500

    Initial commit

---
+++ Project/CustomPopupHost.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MyNamespace {
+  public class CustomPopup : ContentControl {
+    public static readonly DependencyProperty IsOpenProperty = DependencyProperty.Register(
+        nameof(IsOpen), typeof(bool), typeof(CustomPopup), new FrameworkPropertyMetadata(default(bool), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    public bool IsOpen {
+      get => (bool)GetValue(IsOpenProperty);
+      set => SetValue(IsOpenProperty, value);
+    }
+
+    public static readonly DependencyProperty CustomPopupContentProperty = DependencyProperty.Register(
+        nameof(CustomPopupContent), typeof(object), typeof(CustomPopup), new PropertyMetadata(default(object)));
+
+    public object CustomPopupContent {
+      get => GetValue(CustomPopupContentProperty);
+      set => SetValue(CustomPopupContentProperty, value);
+    }
+  }
+}
---
+++ Project/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace MyNamespace {
+  public partial class MainWindow : Window, INotifyPropertyChanged {
+
+    public MainWindow() {
+      DataContext = this;
+      InitializeComponent();
+    }
+
+    private Visibility customPopupContentVisibility = Visibility.Collapsed;
+    public Visibility CustomPopupContentVisibility {
+      get => customPopupContentVisibility;
+      set {
+        customPopupContentVisibility = value;
+        OnPropertyChanged();
+      }
+    }
+
+    private bool isCustomPopupOpen = false;
+    public bool IsCustomPopupOpen {
+      get => isCustomPopupOpen;
+      set {
+        isCustomPopupOpen = value;
+        CustomPopupContentVisibility = value ? Visibility.Visible : Visibility.Collapsed;
+        OnPropertyChanged();
+      }
+    }
+
+    private void OpenCustomPopup(object _1, RoutedEventArgs _2) => IsCustomPopupOpen = true;
+    private void CloseCustomPopup(object _1, RoutedEventArgs _2) => IsCustomPopupOpen = false;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string name = null) =>
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+  }
+}
